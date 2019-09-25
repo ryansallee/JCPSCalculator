6b10c02f9f83d20e0e616a5be996835eb00eab79
Ryan Sallee <rcsall11@g.holycross.edu>
2019-09-25 17:14:42 -0400

    Updated controller class.

--- src/main/java/org/ryansallee/PrimaryController.java
+++ src/main/java/org/ryansallee/PrimaryController.java
@@ -1,6 +1,8 @@
 package org.ryansallee;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.text.Text;
 
 public class PrimaryController {
@@ -11,4 +13,17 @@ public class PrimaryController {
 //    void initialize(){
 //        display.setText("You can do basic math here!");
 //    }
+
+    @FXML
+    private void processNumber(ActionEvent event){
+        String val = ((Button)event.getSource()).getText();
+        display.setText(display.getText() + val);
+    }
+
+    @FXML
+    private void processOperator(ActionEvent event){
+        String val = ((Button)event.getSource()).getText();
+        display.setText(display.getText() + val);
+
+    }
 }
